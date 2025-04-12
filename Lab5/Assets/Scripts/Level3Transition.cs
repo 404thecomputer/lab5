c7e63c22092ef65f8051104fe5522024ed822f19
Jake <blazingpika7@gmail.com>
2025-04-11 21:47:32 -0500

    Fixed Buttons on Title Page

---
+++ Lab5/Assets/Scripts/Level3Transition.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Level3Transition : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ReactToClick() 
+    {
+        // Use the scene fader to load the main scene
+        SceneFader.Instance.FadeToScene("Level3");
+    }
+}
